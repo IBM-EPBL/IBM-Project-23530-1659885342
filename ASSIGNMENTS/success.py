f75bad0877e14e04a14cab1cab10a07150c0b319
Jumana Begum J <87908039+Jumana-Begum-J@users.noreply.github.com>
2022-09-20 21:39:21 +0530

    Add files via upload

---
+++ ASSIGNMENTS/success.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+ 
+app = Flask(__name__) 
+ 
+
+@app.route('/success', methods =["POST"])
+def successpage():
+
+   form_details = []
+   form_details.append(request.form["fname"])
+   form_details.append(request.form["lname"])
+   form_details.append(request.form["dob"])
+   form_details.append(request.form["gender"])
+   form_details.append(request.form["address"])
+   form_details.append(request.form["course"])
+   form_details.append(request.form["email"])
+   form_details.append(request.form["password"])
+   
+   return render_template('success.html', form_details=form_details)
+
+@app.route('/form')
+def studentform():
+    return render_template('form.html')
+ 
+if __name__=='__main__':
+   app.run(debug = True)
